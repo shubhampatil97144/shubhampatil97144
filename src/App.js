aef3cafd8b9e4531e34950b280e1f50f04857544
ajaykoli <ajaymaneshkoli@gmail.com>
2022-03-26 13:11:15 +0530

    fixed issue

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>It`s shubham app</p>
+        <p>Hello frends</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
